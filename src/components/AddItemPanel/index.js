b3db4e9c78ee4e1c0943a1a3c59ae147647ffc63
o.ivankova <o.ivankova@godeltech.com>
2023-05-22 10:59:36 +0200

    PR prepare

--- src/components/AddItemPanel/index.js
+++
@@ -1,3 +0,0 @@
-import AddItemPanel from './AddItemPanel';
-
-export default AddItemPanel;
--- src/components/AppHeader/AppHeader.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./AppHeader.scss";
-
-const AppHeader = () => {
-  return <h1 className="app-header">TO DO</h1>;
-};
-
-export default AppHeader;
